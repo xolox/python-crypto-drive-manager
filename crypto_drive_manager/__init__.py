6ac9658bcc31726710ca90e26aeb7b076dada25a
Peter Odding <peter@peterodding.com>
2017-11-27 23:39:03 +0100

    Integrate linux-utils

--- crypto_drive_manager/__init__.py
+++ crypto_drive_manager/__init__.py
@@ -10,11 +10,13 @@
 import functools
 import logging
 import os
-import re
 
 # External dependencies.
 from executor import execute
 from humanfriendly import pluralize, Timer
+from linux_utils.crypttab import parse_crypttab
+from linux_utils.fstab import find_mounted_filesystems
+from linux_utils.luks import cryptdisks_start
 
 __version__ = '1.0'
 """Semi-standard module versioning."""
@@ -67,9 +69,9 @@ def initialize_keys_device(image_file, mapper_name, mount_point):
                 # Create, install and use the keys to unlock the drives.
                 num_unlocked = 0
                 for device in find_managed_drives(mount_point):
-                    if device['available'] and activate_encrypted_drive(
-                            mapper_name=device['name'],
-                            physical_device=device['physical_device'],
+                    if device.is_available and activate_encrypted_drive(
+                            mapper_name=device.target,
+                            physical_device=device.source_device,
                             keys_directory=mount_point,
                             reset=first_run):
                         num_unlocked += 1
@@ -107,8 +109,10 @@ def activate_encrypted_drive(mapper_name, physical_device, keys_directory, reset
         os.chmod(key_file, 0o400)
         if not device_exists:
             logger.info("Unlocking encrypted drive %s ..", mapper_name)
-            if not execute('cryptdisks_start', mapper_name, check=False):
-                logger.warning("Failed to unlock encrypted drive %s!", mapper_name)
+            try:
+                cryptdisks_start(mapper_name)
+            except Exception as e:
+                logger.warning("Failed to unlock encrypted drive %s! (%s)", mapper_name, e)
                 return False
     if drive_needs_mounting(mapper_device):
         logger.info("Mounting %s ..", mapper_device)
@@ -120,40 +124,16 @@ def activate_encrypted_drive(mapper_name, physical_device, keys_directory, reset
 
 def find_managed_drives(keys_directory):
     """
-    Find the encrypted drives that ``crypto-drive-manager`` manages.
+    Find the encrypted drives managed by `crypto-drive-manager`.
 
-    :param keys_directory: The mount point for the virtual keys device (a
-                           string).
-    :returns: A generator of dictionaries with the following device properties:
-
-              - ``name``: The device mapper name (a string).
-              - ``key_file``: The pathname of the key file (a string or
-                              ``None``).
-              - ``options``: A :py:class:`set` of options (strings).
-              - ``available``: ``True`` if the physical drive is available,
-                               ``False`` otherwise.
-              - ``physical_device``: The pathname of the physical drive (this
-                                     key/value pair is only available if
-                                     ``available=True``).
+    :param keys_directory: The mount point for the virtual keys device (a string).
+    :returns: A generator of :class:`~linux_utils.crypttab.EncryptedFileSystemEntry` objects.
     """
-    for tokens in parse_tab_file('/etc/crypttab'):
-        if len(tokens) == 4:
-            mapper_name, location, key_file, options = tokens
-            common_prefix = os.path.commonprefix([keys_directory, key_file])
-            tokenized_options = set(filter(None, options.split(',')))
-            if 'luks' in tokenized_options and common_prefix == keys_directory:
-                properties = dict(name=mapper_name,
-                                  key_file=key_file if key_file != 'none' else None,
-                                  options=tokenized_options,
-                                  available=False)
-                # Find the physical device.
-                name, _, value = location.partition('=')
-                if name.upper() == 'UUID':
-                    disk_by_uuid = '/dev/disk/by-uuid/%s' % value.lower()
-                    if os.path.exists(disk_by_uuid):
-                        properties['available'] = True
-                        properties['physical_device'] = os.path.realpath(disk_by_uuid)
-                yield properties
+    for entry in parse_crypttab():
+        if 'luks' in entry.options and entry.key_file:
+            common_prefix = os.path.commonprefix([keys_directory, entry.key_file])
+            if common_prefix == keys_directory:
+                yield entry
 
 
 def drive_needs_mounting(mapper_device):
@@ -163,7 +143,7 @@ def drive_needs_mounting(mapper_device):
     :param mapper_device: The pathname of the device mapper device (a string).
     :returns: ``True`` if the drive should be mounted, ``False`` otherwise.
     """
-    if mapper_device in find_mounted_devices():
+    if any(fs.device_file == mapper_device for fs in find_mounted_filesystems()):
         logger.debug("Drive %s is already mounted.", mapper_device)
         return False
     for line in execute('blkid', '-o', 'export', mapper_device, capture=True).splitlines():
@@ -173,36 +153,6 @@ def drive_needs_mounting(mapper_device):
             return False
     logger.debug("Drive %s not yet mounted.", mapper_device)
     return True
-
-
-def find_mounted_devices():
-    """
-    Find the mounted devices.
-
-    :returns: A generator of device pathnames (strings).
-    """
-    for tokens in parse_tab_file('/etc/mtab'):
-        yield tokens[0]
-
-
-def parse_tab_file(pathname):
-    """
-    Parse UNIX configuration files like ``/etc/fstab`` and ``/etc/crypttab``.
-
-    Automatically strips comments and tokenizes input by splitting on whitespace.
-
-    :param pathname: The absolute pathname of the configuration file (a
-                     string).
-    :returns: A generator of lists with tokenized lines.
-    """
-    with open(pathname) as handle:
-        for line in handle:
-            # Strip comments.
-            line = re.sub('#.*', '', line)
-            # Tokenize input.
-            tokens = line.split()
-            if tokens:
-                yield tokens
 
 
 class finalizer(object):
